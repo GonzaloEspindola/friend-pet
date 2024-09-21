1ea13f1aa0e367e5f55c8f8a336bb34a3a26635a
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2024-09-20 21:25:39 -0300

    fix color card

--- components/MyPets/MyPetsCard.vue
+++ components/MyPets/MyPetsCard.vue
@@ -20,7 +20,7 @@ const openEditModal = (id: string) => {
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 p-2 bg-white rounded-md drop-shadow-md">
+  <div class="flex flex-col gap-2 p-2 bg-base-300 rounded-md drop-shadow-md">
     <MyPetsImage :url="props.imageUrl" />
     <article class="flex justify-between items-center gap-2">
       <MyPetsCardActionsShowQR :id="props.id" :qr-code="props.qrCode" />
